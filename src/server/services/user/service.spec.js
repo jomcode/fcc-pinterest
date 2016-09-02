15bf2e04dc99fcba02ed7e42bb4d8d7a92db857e
John <jomcode@gmail.com>
2016-09-02 10:11:21 -0500

    setting up server user service database service test

--- src/server/services/user/service.spec.js
+++ src/server/services/user/service.spec.js
@@ -4,14 +4,35 @@ const UserService = require('./service');
 
 describe('User Service database service', () => {
   describe('#get', () => {
+    function fakeDb() {}
+
+    fakeDb.prototype.query = function query(cypher, params, cb) {};
+
+    const db = new fakeDb();
+    const service = new UserService(db);
+
     it('returns expected data');
   });
 
   describe('#create', () => {
+    function fakeDb() {}
+
+    fakeDb.prototype.query = function query(cypher, params, cb) {};
+
+    const db = new fakeDb();
+    const service = new UserService(db);
+
     it('returns expected data');
   });
 
   describe('#remove', () => {
+    function fakeDb() {}
+
+    fakeDb.prototype.query = function query(cypher, params, cb) {};
+
+    const db = new fakeDb();
+    const service = new UserService(db);
+
     it('returns expected data');
   });
 });
