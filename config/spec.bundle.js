40c9c0e52959a4dab682d24363681a35b0ebed1c
John <jomcode@gmail.com>
2016-09-05 05:19:14 -0500

    client test config changes
    added polyfills to client bundle for testing. added fetch-mock for tests

--- config/spec.bundle.js
+++ config/spec.bundle.js
@@ -1,4 +1,5 @@
-require('core-js');
+require('core-js'); // es6/7 polyfills
+require('whatwg-fetch'); // fetch polyfill
 global.sinon = require('sinon');
 
 const context = require.context('../src/client', true, /\.spec\.js$/);
